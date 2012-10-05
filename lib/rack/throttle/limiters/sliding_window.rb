fba99918ab5de3715167fc5beb1be378b4aa3db5
Ben Somers <somers.ben@gmail.com>
2012-10-05 11:17:24 -0700

    Adding experimental sliding window

---
+++ lib/rack/throttle/limiters/sliding_window.rb
@@ -0,0 +1,76 @@
+module Rack; module Throttle
+  ##
+  # This rate limiter strategy throttles the application with
+  # a sliding window (implemented as a leaky bucket). It operates
+  # on second-level resolution. It takes :burst and :average 
+  # options, which correspond to the maximum size of a traffic 
+  # burst, and the maximum allowed average traffic level.
+  class SlidingWindow < Limiter
+    ##
+    # @param  [#call]                    app
+    # @param  [Hash{Symbol => Object}]   options
+    # @option options [Integer] :burst   5
+    # @option options [Float] :average   1
+    def initialize(app, options = {})
+      super
+      options[:burst] ||= 5
+      options[:average] ||= 1
+    end
+
+    ##
+    # Returns `true` if sufficient time (equal to or more than
+    # {#minimum_interval}) has passed since the last request and the given
+    # present `request`.
+    #
+    # @param  [Rack::Request] request
+    # @return [Boolean]
+    def allowed?(request)
+      t1 = request_start_time(request)
+      key = cache_key(request)
+      bucket = cache_get(key) rescue nil
+      bucket ||= LeakyBucket.new(options[:burst], options[:average])
+      bucket.leak! 
+      bucket.increment!
+      allowed = !bucket.overfull?
+      begin
+        cache_set(key, bucket)
+        allowed
+      rescue StandardError => e
+        allowed = true
+        # If an error occurred while trying to update the timestamp stored
+        # in the cache, we will fall back to allowing the request through.
+        # This prevents the Rack application blowing up merely due to a
+        # backend cache server (Memcached, Redis, etc.) being offline.
+      end
+    end
+
+    class LeakyBucket
+      attr_reader :count, :maximum, :outflow, :last_touched
+
+      def initialize(maximum, outflow)
+        @maximum, @outflow = maximum, outflow
+        @count, @last_touched = 0, Time.now
+      end
+
+      def leak!
+        t = Time.now
+        time = t - last_touched
+        loss = (outflow * time).to_f
+        if loss > 0
+          @count -= loss
+          @last_touched = t 
+        end
+      end
+
+      def increment!
+        @count = 0 if count < 0
+        @count += 1
+      end
+
+      def overfull?
+        count > maximum
+      end
+    end
+
+  end
+end; end
---
+++ spec/limiters/sliding_window_spec.rb
@@ -0,0 +1,67 @@
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
+
+describe Rack::Throttle::SlidingWindow do
+  include Rack::Test::Methods
+
+  def app
+    @target_app ||= example_target_app
+    @app ||= Rack::Throttle::SlidingWindow.new(@target_app, :burst => 2, :average => 1)
+  end
+
+  before(:each) do
+    @time = Time.now
+    Timecop.freeze
+  end
+
+  after(:each) do
+    Timecop.return
+  end
+
+  it "should allow the request if the source has not been seen at all" do
+    get "/foo"
+    last_response.body.should show_allowed_response
+  end
+  
+  it "should allow the request if the rate is above-average but within the burst rule" do
+    Timecop.freeze(@time) { get "/foo" }
+    Timecop.freeze(@time + 0.5) { get "/foo" }
+    last_response.body.should show_allowed_response
+  end
+
+   it "should not allow the request if the rate is greater than the burst rule" do
+    Timecop.freeze(@time) { get "/foo" }
+    Timecop.freeze(@time + 0.3) { get "/foo" }
+    Timecop.freeze(@time + 0.6) { get "/foo" }
+    last_response.body.should show_throttled_response
+  end
+  
+  it "should allow the request if the rate is less than the average" do
+    Timecop.freeze(@time) { get "/foo" }
+    Timecop.freeze(@time + 0.5) { get "/foo" }
+    Timecop.freeze(@time + 2) { get "/foo" }
+
+    last_response.body.should show_allowed_response
+  end
+
+  it "should not allow the request if the rate is more than the average" do
+    Timecop.freeze(@time) { get "/foo" }
+    Timecop.freeze(@time + 0.5) { get "/foo" }
+    Timecop.freeze(@time + 1) { get "/foo" }
+    Timecop.freeze(@time + 1.5) { get "/foo" }
+
+    last_response.body.should show_throttled_response
+  end
+
+  it "should gracefully allow the request if the cache bombs on getting" do
+    app.should_receive(:cache_get).and_raise(StandardError)
+    get "/foo"
+    last_response.body.should show_allowed_response
+  end
+  
+  it "should gracefully allow the request if the cache bombs on setting" do
+    app.should_receive(:cache_set).and_raise(StandardError)
+    get "/foo"
+    last_response.body.should show_allowed_response
+  end
+end
+
